b39e0f9304fe8619b32759ac8c403e7e61cae38a
esburch <burch.emilys@gmail.com>
2017-07-28 23:18:00 -0400

    A little better logic

--- scripts/aws_upload.sh
+++ scripts/aws_upload.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -ev
-if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
-  S3_BUCKET="pr.emilyburch.com"
-  echo "Pull Request Build: Deploy to $S3_BUCKET"
+
+if  [ "${TRAVIS_BRANCH}" = "master" ]; then
+  if  [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
+    S3_BUCKET="pr.emilyburch.com"
+    echo "Pull Request Build: Deploy to $S3_BUCKET"
+  else
+    S3_BUCKET="emilyburch.com"
+    echo "${TRAVIS_BRANCH} Build: Deploy to $S3_BUCKET"
+  fi
 elif [ "${TRAVIS_BRANCH}" = "develop" ]; then
   S3_BUCKET="test.emilyburch.com"
   echo "${TRAVIS_BRANCH} Build: Deploy to $S3_BUCKET"
-elif [ "${TRAVIS_BRANCH}" = "master" ]; then
-  S3_BUCKET="emilyburch.com"
-  echo "${TRAVIS_BRANCH} Build: Deploy to $S3_BUCKET"
-  INVALIDATE="true"
 fi
 
 aws s3 sync --acl "public-read" --sse "AES256" public/ s3://$S3_BUCKET
